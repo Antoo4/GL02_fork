f3a6d7ce90a5e2d346ea139ea1455e4c14793c8d
Thomas CHARPENTIER <thomas.charpentier@utt.fr>
2025-12-07 17:22:40 +0100

    Reset Main Branch and Add Gitignore

--- command.js
+++
@@ -1,9 +0,0 @@
-var Commands = {
-    check: function(){
-        console.log("Commands Ready !")
-        console.log("Magenta level checked".magenta)
-        return true;
-    }
-}
-
-module.exports = Commands
--- radio.js
+++
@@ -1,8 +0,0 @@
-var Radio = {
-    check: function(){
-        console.log("Radio Ready !")
-        console.log("Magenta level checked".magenta)
-        return true
-    }
-}
-module.exports = Radio
